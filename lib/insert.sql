99957ee6ba463db5c704d55d8629f29bb0dc364f
DanFCo <danfcorreia@gmail.com>
2019-03-18 08:04:30 -0400

    not your average bear

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Mr.Chocolate",32,"male","brown","sweet",1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Rowdy",23,"male","red","hostile",0);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Tabitha",43,"female","grey","calm",1);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",65,"male","grey","hostile",1);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",34,"female","brown","shy",1);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",99,"male","green","nasty",0);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",54,"female","golden brown","calm",1);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,43,"male","brown","agressive",0);
