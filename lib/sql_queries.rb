648d29d3f89190f859c1ee2cab17d180a5e7c5a3
DanFCo <danfcorreia@gmail.com>
2019-03-18 11:58:43 -0400

    da bears da bears da bears

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,8 +20,6 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1"
-
-
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -29,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name IS NULL;"
+  "SELECT * FROM bears WHERE name IS NULL"
 end
